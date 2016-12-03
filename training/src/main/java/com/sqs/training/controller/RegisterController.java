5c0a21f6cfb177653520dd247756a4c11184976f
nathanhunter <nathaniel.hunter@uky.edu>
2016-12-03 00:43:51 -0500

    Update RegisterController.java

--- training/src/main/java/com/sqs/training/controller/RegisterController.java
+++ training/src/main/java/com/sqs/training/controller/RegisterController.java
@@ -2,9 +2,11 @@ package com.sqs.training.controller;
 
 import java.util.Map;
 
+import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sqs.training.domain.User;
 
@@ -22,5 +24,18 @@ public class RegisterController {
 	public String registerUser(@ModelAttribute("registrationForm") User user) {
 		System.out.println(user.getFirstName() + " " + user.getLastName());
 		return "register";
+	}
+	
+	@RequestMapping("/device")
+	public @ResponseBody String detectDevice(String id, Device device) {
+		String deviceType = "unknown";
+		if (device.isNormal()) {
+			deviceType = "normal";
+		} else if (device.isMobile()) {
+			deviceType = "mobile";
+		} else if (device.isTablet()) {
+			deviceType = "tablet";
+		}
+		return deviceType; 
 	}
 } 
