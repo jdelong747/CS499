d92329165d6d5a6d26ba036cd366cfda1183a5a4
Josh <josh.delong@uky.edu>
2016-12-03 13:53:55 -0500

    Added device recognition to return mobile pages if on a mobile device

--- training/src/main/java/com/sqs/training/controller/DefaultController.java
+++ training/src/main/java/com/sqs/training/controller/DefaultController.java
@@ -4,26 +4,16 @@ import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
 
 	@RequestMapping(value = {"/home", "/"})
-	public String displayHomePage(Model model) {
-		return "home";
-	}
-	
-	@RequestMapping("/device")
-	public @ResponseBody String detectDevice(String id, Device device) {
-		String deviceType = "unknown";
-		if (device.isNormal()) {
-			deviceType = "normal";
-		} else if (device.isMobile()) {
-			deviceType = "mobile";
-		} else if (device.isTablet()) {
-			deviceType = "tablet";
+	public String displayHomePage(Model model, Device device) {
+		if (device.isMobile()) {
+			return "mobile/home";
+		} else {
+			return "home";
 		}
-		return deviceType; 
 	}
 }
