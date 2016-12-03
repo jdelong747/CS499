95f99e1b45115f31e23678c7badaf63b52fd0fc8
nathanhunter <nathaniel.hunter@uky.edu>
2016-12-03 00:41:35 -0500

    Update DefaultController.java

--- training/src/main/java/com/sqs/training/controller/DefaultController.java
+++ training/src/main/java/com/sqs/training/controller/DefaultController.java
@@ -1,8 +1,10 @@
 package com.sqs.training.controller;
 
+import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
@@ -10,5 +12,18 @@ public class DefaultController {
 	@RequestMapping(value = {"/home", "/"})
 	public String displayHomePage(Model model) {
 		return "home";
+	}
+	
+	@RequestMapping("/device")
+	public @ResponseBody String detectDevice(String id, Device device) {
+		String deviceType = "unknown";
+		if (device.isNormal()) {
+			deviceType = "normal";
+		} else if (device.isMobile()) {
+			deviceType = "mobile";
+		} else if (device.isTablet()) {
+			deviceType = "tablet";
+		}
+		return deviceType; 
 	}
 }
